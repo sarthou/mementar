600491fd2e9512cb27714f52902e721b433c3438
Guillaume Sarthou <gsarthou@laas.fr>
2024-06-26 10:18:27 +0200

    [Test] set config test more generic

--- src/test/config.cpp
+++ src/test/config.cpp
@@ -1,26 +1,28 @@
-#include "mementar/core/Parametrization/Configuration.h"
-
 #include <iostream>
+#include <string>
+
+#include "mementar/compat/ros.h"
+#include "mementar/core/Parametrization/Configuration.h"
 
 int main()
 {
   mementar::Configuration config;
-  bool ok = true;
-  ok = config.read("/home/gsarthou/Robots/Pr2/Semantic/catkin_ws/src/mementar/file_intern/configuration.yaml");
+  std::string path_base = mementar::compat::mem_ros::getShareDirectory("mementar");
+  bool ok = config.read(path_base + "/files/config_example.yaml");
   if(ok)
   {
-    if(config.exist("WhiteListe"))
+    if(config.exist("whitelist"))
     {
-      config["WhiteListe"].push_back("new property");
+      config["whitelist"].pushBack("new property");
       config.display();
-      ok = config.write("/home/gsarthou/Robots/Pr2/Semantic/catkin_ws/src/mementar/file_intern/new_configuration.yaml");
+      ok = config.write(path_base + "/file_intern/new_configuration.yaml");
       if(!ok)
-        std::cout << "fail to write the new configuation file" << std::endl;
+        std::cout << "fail to write the new configuation file: " << path_base << "/file_intern/new_configuration.yaml" << std::endl;
     }
     else
-      std::cout << "WhiteListe not found" << std::endl;
+      std::cout << "whitelist not found" << std::endl;
   }
   else
-    std::cout << "fail to read configuation file" << std::endl;
+    std::cout << "fail to read configuation file: " << path_base << "/files/config_example.yaml" << std::endl;
   return 0;
 }
